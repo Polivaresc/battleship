550722428f5b7716e1f6416a8f604959eca625ae
Polivaresc <paulaolca98@gmail.com>
2022-02-14 22:04:28 +0100

    ship unit tests

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,52 @@
-function shipFactory (len, hit = null, sink = false) {
+function shipFactory (initial_pos, final_pos) {
+    const hitPositions = []
+    const len = calculateLength(initial_pos, final_pos)
+    const allPositions = getAllPositions(initial_pos, final_pos, len)
 
-    return { len, hit, sink }
+    function hit(pos) {
+        if (hitPositions.includes(pos)) {
+            throw Error('This position is already hit')
+        }
+        allPositions.includes(pos) ? hitPositions.push(pos) : null
+    }
+
+    function isSunk() {
+        const notHitPositions = allPositions.filter(p => !hitPositions.includes(p))
+        return notHitPositions.length === 0
+    }
+
+    return { len, hitPositions, isSunk, hit }
+}
+
+
+
+
+
+function calculateLength(initial_pos, final_pos) {
+    const start = initial_pos.split('')
+    const end = final_pos.split('')
+    return (end[0].charCodeAt(0) - start[0].charCodeAt(0)) + (end[1] - start[1]) + 1
+}
+
+function getAllPositions(initial_pos, final_pos, len) {
+    const start = initial_pos.split('')
+    const end = final_pos.split('')
+
+    const direction = end[1] - start[1] > 0 ? 'vertical' : 'horizontal'
+    const allPositions = []
+
+    allPositions.push(initial_pos)
+    let current_pos = initial_pos
+    for(let i = 1; i < len; i++) {
+        let split_current = current_pos.split('')
+        current_pos = direction === 'vertical' 
+            ? split_current[0] + (parseInt(split_current[1]) + 1)                           // Add 1 to right coord A1 -> A2
+            : String.fromCharCode(split_current[0].charCodeAt(0) + 1) + split_current[1]    // Add 1 to left coord A1 -> B1
+
+        allPositions.push(current_pos)
+    }
+
+    return allPositions
 }
 
 module.exports = {
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,10 +1,45 @@
 const shipModule = require('./ship')
 const ship = shipModule.shipFactory
 
-test('ships contain len, hit and sink', () => {
-    expect(ship(1)).toEqual({len: 1, hit: null, sink: false})
+test('can create new ship of len 1', () => {
+    const newShip = ship('A1', 'A1')
+    expect(newShip.len).toBe(1)
 })
 
-// test('hit() function returns hit position', () => {
-//     expect(ship(4)).toEqual({len: 4, hit: 3, sink: false})
-// })
+test('can create new ship of len 2', () => {
+    const newShip = ship('A1', 'B1')
+    expect(newShip.len).toBe(2)
+})
+
+test('hit a ship and assert position hit', () => {
+    const newShip = ship('B2', 'B5')
+    newShip.hit('B3')
+    expect(newShip.hitPositions).toContain('B3')
+})
+
+test('miss a hit and assert no position hit', () => {
+    const newShip = ship('A2', 'A2')
+    newShip.hit('F4')
+    expect(newShip.hitPositions).not.toContain('F4')
+})
+
+test('cannot re-hit position', () => {
+    const newShip = ship('A2', 'C2')
+    newShip.hit('B2')
+    expect(newShip.hitPositions).toContain('B2')
+    expect(() => newShip.hit('B2')).toThrow('This position is already hit')
+})
+
+test('hit all positions of ship and assert isSunk is true', () => {
+    const newShip = ship('A1', 'A3')
+    newShip.hit('A3')
+    newShip.hit('A1')
+    newShip.hit('A2')
+    expect(newShip.isSunk()).toBe(true)
+})
+
+test('hit some positions of ship and assert isSunk is false', () => {
+    const newShip = ship('A1', 'A3')
+    newShip.hit('A1')
+    expect(newShip.isSunk()).toBe(false)
+})
